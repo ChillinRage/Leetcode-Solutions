cb87547f544210941aded4355e42305ef46c02b6
ChillinRage <73763719+ChillinRage@users.noreply.github.com>
2023-01-05 20:13:12 +0800

    Update Permutations II.py

--- 2. Medium/Permutations II.py
+++ 2. Medium/Permutations II.py
@@ -1,2 +1,26 @@
-def permuteUnique(nums):
-        return list(set(i for i in itertools.permutations(nums)))
+#Solution 1 (cheese strat)
+def permuteUnique1(nums):
+    return list(set(i for i in itertools.permutations(nums)))
+
+#Solution 2 (intended approach)
+def permuteUnique2(nums):
+    output   = []
+    main_len = len(nums)
+        
+    def backtrack(current, counter, length):
+        if length == main_len:
+            output.append(list(current))
+            return
+            
+        for num in counter:
+            if counter[num] > 0:
+                current.append(num)
+                counter[num] -= 1
+                backtrack(current, counter, length + 1)
+                    
+                current.pop()
+                counter[num] += 1
+        
+    backtrack([], Counter(nums), 0)
+        
+    return output
